46fc06f5f723eeb5cd7e27bd2dea987f33428636
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-16 17:26:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,6 +7,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   elsif on_line > 0
     position=1
-    katz_deli.each {|name| puts "The line is currently: #{position}. #{name}"; position += 1}
+    puts "The line is currently: "
+    katz_deli.each {|name| puts "#{position}. #{name}"; position += 1}
   end
 end
