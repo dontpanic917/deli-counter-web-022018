e2c1a3ba65a25adf49a615b317c15ab5f9488353
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-16 17:46:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(katz_deli)
   elsif on_line > 0
     list=[]
     position=1
-    katz_deli.each {|name| (position < online+1) ? list << "#{position}. #{name} " : list << "#{position}. #{name}" ; position += 1}
+    katz_deli.each {|name| (position < on_line+1) ? list << "#{position}. #{name} " : list << "#{position}. #{name}" ; position += 1}
     puts "The line is currently: #{list.each {|element| return element; return " " }}"
   end
 end
