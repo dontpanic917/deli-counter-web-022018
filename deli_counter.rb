b064abb8edce61535529356876565fb357715da3
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-16 17:22:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ on_line = 0
 def line(katz_deli)
   on_line=katz_deli.size
   if on_line == 0
-    puts "the line is empty"
+    puts "The line is currently empty."
   elsif on_line > 0
     puts katz_deli
   end
