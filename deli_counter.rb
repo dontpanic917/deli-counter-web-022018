2d3c31e02c7a79b3d692643b09804f5f07a98830
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-16 17:52:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,22 +1,20 @@
 # Write your code here.
 katz_deli = []
-on_line = 0
 
 def line(katz_deli)
-  on_line=katz_deli.size
-  if on_line == 0
+
+  if katz_deli.size == 0
     puts "The line is currently empty."
-  elsif on_line > 0
+  elsif katz_deli.size > 0
     list=""
     position=1
-    katz_deli.each {|name| (position < on_line) ? list << "#{position}. #{name} " : list << "#{position}. #{name}" ; position += 1}
+    katz_deli.each {|name| (position < katz_deli.size) ? list << "#{position}. #{name} " : list << "#{position}. #{name}" ; position += 1}
     puts "The line is currently: #{list}"
   end
 end
 
-on_line = 0
 def take_a_number(katz_deli, person)
-  if on_line == 0
+  if katz_deli.size == 0
     katz_deli << person
   end
 end
